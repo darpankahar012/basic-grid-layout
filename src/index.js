01c2b31e3c8c2e008bb4620dffb6ca77b31b11ab
darpan <darpan@webmynesystems.com>
2022-11-14 12:34:10 +0530

    basic setup react-grid-layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import AddRemoveLayout from "./ShowcaseLayout";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class ExampleLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { layout: [] };
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  onLayoutChange(layout) {
+    this.setState({ layout: layout });
+  }
+
+  stringifyLayout() {
+    return this.state.layout.map(function (l) {
+      return (
+        <div className="layoutItem" key={l.i}>
+          <b>{l.i}</b>: [{l.x}, {l.y}, {l.w}, {l.h}]
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="layoutJSON">
+          Displayed as <code>[x, y, w, h]</code>:
+          <div className="columns">{this.stringifyLayout()}</div>
+        </div>
+        <AddRemoveLayout onLayoutChange={this.onLayoutChange} />
+      </div>
+    );
+  }
+}
+
+const contentDiv = document.getElementById("root");
+const gridProps = window.gridProps || {};
+ReactDOM.render(React.createElement(ExampleLayout, gridProps), contentDiv);
